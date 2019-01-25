5ca0ff1148f7ef8e9282c70ebd5c178a01b88418
Luke McDonald <lukekm.mcdonald@gmail.com>
2019-01-25 21:25:54 +0000

    Making script more robust

--- docker/setup/disable-plugins.sh
+++ docker/setup/disable-plugins.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-declare -a plugins=("enter-plugin-name")
+
+######################
+# disable-plugins.sh #
+######################
+#
+# Description: Disables plugins as defined in `plugins` variable, by renaming the folder.
+#
+
 plugin_path="/var/www/html/wp-content/plugins"
+declare -a plugins=("enter-plugin-1" "enter-plugin-2" "enter-plugin-3")
 
 for i in "${plugins[@]}"
 do
-    echo "Disabling $i ..."
-    mv "${plugin_path}/$i" "${plugin_path}/${i}_DISABLED"
+    echo "--- Attempting to disable $i ---"
+        if [ -d "${plugin_path}/${i}_DISABLED" ]; then
+            echo "'$i' is already disabled"
+        elif [ -d "${plugin_path}/$i" ]; then
+            mv "${plugin_path}/$i" "${plugin_path}/${i}_DISABLED"
+            echo "'$i' successfully disabled"
+        else
+            echo "'$i' doesn't exist, so cannot be disabled"
+        fi
+    echo "--- end $i ---"
 done
