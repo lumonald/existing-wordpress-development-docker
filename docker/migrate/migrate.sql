b779870f1ec97598aebc3e4cae93b2e01afb8b2e
Luke McDonald <lukekm.mcdonald@gmail.com>
2019-01-25 20:55:45 +0000

    Making query syntax consistant

--- docker/migrate/migrate.sql
+++ docker/migrate/migrate.sql
@@ -1,7 +1,7 @@
 -- This script is used to change the domain name in the database, so the site can run locally
-UPDATE your_prefix_options SET option_value = replace(option_value, 'http://yourdomain.com', 'http://127.0.0.1') WHERE option_name = 'home' OR option_name = 'siteurl';
-UPDATE your_prefix_posts SET guid = replace(guid, 'http://yourdomain.com','http://127.0.0.1');
-UPDATE your_prefix_posts SET post_content = REPLACE (post_content, 'http://yourdomain.com', 'http://127.0.0.1');
-UPDATE your_prefix_posts SET post_content = REPLACE (post_content, 'src=\"http://yourdomain.com', 'src=\"http://127.0.0.1');
-UPDATE your_prefix_posts SET guid = REPLACE (guid, 'http://yourdomain.com', 'http://127.0.0.1') WHERE post_type = 'attachment';
-UPDATE your_prefix_postmeta SET meta_value = REPLACE (meta_value, 'http://yourdomain.com','http://127.0.0.1');
+UPDATE your_prefix_options SET option_value = REPLACE(option_value, 'http://yourdomain.com', 'http://127.0.0.1') WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE your_prefix_posts SET guid = REPLACE(guid, 'http://yourdomain.com','http://127.0.0.1');
+UPDATE your_prefix_posts SET post_content = REPLACE(post_content, 'http://yourdomain.com', 'http://127.0.0.1');
+UPDATE your_prefix_posts SET post_content = REPLACE(post_content, 'src=\"http://yourdomain.com', 'src=\"http://127.0.0.1');
+UPDATE your_prefix_posts SET guid = REPLACE(guid, 'http://yourdomain.com', 'http://127.0.0.1') WHERE post_type = 'attachment';
+UPDATE your_prefix_postmeta SET meta_value = REPLACE(meta_value, 'http://yourdomain.com','http://127.0.0.1');
